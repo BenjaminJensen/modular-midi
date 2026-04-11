1f94633b1ed26442a8a83d0282511cabdf1d1b15
Benjamin Jensen <ben@tamam.dk>
2026-04-11 10:49:01 +0200

    build.ps1 changed for workstation, other env solution needed

--- build.ps1
+++ build.ps1
@@ -1,18 +1,20 @@
-$ARM_PATH = "C:\Program Files (x86)\arm-gnu-toolchain\bin"
+$ARM_PATH = "C:\Program Files (x86)\Arm GNU Toolchain arm-none-eabi\14.3 rel1\bin"
 $NINJA_PATH = "C:\Program Files (x86)\ninja"
-$CMAKE_PATH = "C:\Program Files (x86)\cmake-4.3.0-rc2-windows-x86_64\bin"
+$CMAKE_PATH = "C:\Program Files\CMake\binn"
 
 $env:PATH = "$ARM_PATH;$NINJA_PATH;$CMAKE_PATH;" + $env:PATH
-$env:PICO_SDK_PATH = "C:\projects\modular_midi\libs\pico-sdk"
+$env:PICO_SDK_PATH = "F:\git\electronics\modular-midi\libs\pico-sdk"
 
 # This flag tells the SDK: "Don't try to sign the binary, I'll handle it."
-$BUILD_DIR = "C:\projects\modular_midi\build"
+$BUILD_DIR = "F:\git\electronics\modular-midi\build"
 if (!(Test-Path $BUILD_DIR)) { New-Item -ItemType Directory -Path $BUILD_DIR }
 
 cd $BUILD_DIR
 
 # Configure without the 'extra outputs' that trigger the picotool build
-cmake -G "Ninja" -DPICO_BOARD=pico2 -DPICO_NO_PICOTOOL=1 ..
+cmake -G "Ninja" -DPICO_BOARD=pico2 -DPICO_NO_PICOTOOL=1 -DPICO_PLATFORM=rp2350..
 
 # Build with your 24 threads
 ninja -j 24
+
+cd ..
